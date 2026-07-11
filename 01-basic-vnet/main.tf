0320493784a65a1d7e068f31387c82e4f135b0af
Nathan Z . Albert <76236654+nathanzalbert@users.noreply.github.com>
2026-07-10 22:22:51 -0400

    Add Terraform configuration for Azure resources

--- 01-basic-vnet/main.tf
+++ 01-basic-vnet/main.tf
@@ -1,1 +1,37 @@
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = var.virtual_network_address_space
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.subnet_address_prefixes
+}
