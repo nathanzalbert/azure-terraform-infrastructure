c875807ed296a0522a8fdce1ce46c31e8e06ce90
Nathan Z . Albert <76236654+nathanzalbert@users.noreply.github.com>
2026-07-11 09:40:48 -0400

    Add Terraform configuration for network security groups

---
+++ 02-network-security-groups/main.tf
@@ -0,0 +1,123 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Creates the resource group.
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
+
+# Creates the virtual network.
+resource "azurerm_virtual_network" "main" {
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = var.virtual_network_address_space
+
+  tags = var.tags
+}
+
+# Creates the web subnet.
+resource "azurerm_subnet" "web" {
+  name                 = var.web_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.web_subnet_address_prefixes
+}
+
+# Creates the management subnet.
+resource "azurerm_subnet" "management" {
+  name                 = var.management_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.management_subnet_address_prefixes
+}
+
+# Creates the NSG used by the web subnet.
+resource "azurerm_network_security_group" "web" {
+  name                = var.web_nsg_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = var.tags
+}
+
+# Allows inbound HTTP traffic from the internet.
+resource "azurerm_network_security_rule" "allow_http" {
+  name                        = "Allow-HTTP-Inbound"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.web.name
+}
+
+# Allows inbound HTTPS traffic from the internet.
+resource "azurerm_network_security_rule" "allow_https" {
+  name                        = "Allow-HTTPS-Inbound"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.web.name
+}
+
+# Creates the NSG used by the management subnet.
+resource "azurerm_network_security_group" "management" {
+  name                = var.management_nsg_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = var.tags
+}
+
+# Allows SSH only from the trusted public IP address.
+resource "azurerm_network_security_rule" "allow_ssh" {
+  name                        = "Allow-SSH-From-Trusted-IP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = var.trusted_ip_address
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.management.name
+}
+
+# Associates the web NSG with the web subnet.
+resource "azurerm_subnet_network_security_group_association" "web" {
+  subnet_id                 = azurerm_subnet.web.id
+  network_security_group_id = azurerm_network_security_group.web.id
+}
+
+# Associates the management NSG with the management subnet.
+resource "azurerm_subnet_network_security_group_association" "management" {
+  subnet_id                 = azurerm_subnet.management.id
+  network_security_group_id = azurerm_network_security_group.management.id
+}
