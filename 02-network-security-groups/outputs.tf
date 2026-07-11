1b7475a3c16a70274f0a113d09365ea8e667dbce
Nathan Z . Albert <76236654+nathanzalbert@users.noreply.github.com>
2026-07-11 09:41:36 -0400

    Define outputs for network security groups and resources
    Added outputs for resource group, virtual network, subnets, and network security groups.

---
+++ 02-network-security-groups/outputs.tf
@@ -0,0 +1,39 @@
+output "resource_group_name" {
+  description = "Name of the created Resource Group."
+  value       = azurerm_resource_group.main.name
+}
+
+output "virtual_network_name" {
+  description = "Name of the created Virtual Network."
+  value       = azurerm_virtual_network.main.name
+}
+
+output "web_subnet_id" {
+  description = "Resource ID of the web subnet."
+  value       = azurerm_subnet.web.id
+}
+
+output "management_subnet_id" {
+  description = "Resource ID of the management subnet."
+  value       = azurerm_subnet.management.id
+}
+
+output "web_network_security_group_name" {
+  description = "Name of the web Network Security Group."
+  value       = azurerm_network_security_group.web.name
+}
+
+output "web_network_security_group_id" {
+  description = "Resource ID of the web Network Security Group."
+  value       = azurerm_network_security_group.web.id
+}
+
+output "management_network_security_group_name" {
+  description = "Name of the management Network Security Group."
+  value       = azurerm_network_security_group.management.name
+}
+
+output "management_network_security_group_id" {
+  description = "Resource ID of the management Network Security Group."
+  value       = azurerm_network_security_group.management.id
+}
