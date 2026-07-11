a1da8fa6a65221159075290bb32fcbf83be34d4e
Nathan Z . Albert <76236654+nathanzalbert@users.noreply.github.com>
2026-07-10 22:28:11 -0400

    Define variables for Azure resources in Terraform
    Added variables for resource group, location, virtual network, subnet, and tags.

--- 01-basic-vnet/variables.tf
+++ 01-basic-vnet/variables.tf
@@ -1,1 +1,39 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group."
+  type        = string
+}
 
+variable "location" {
+  description = "Azure region where resources will be created."
+  type        = string
+}
+
+variable "virtual_network_name" {
+  description = "Name of the Azure Virtual Network."
+  type        = string
+}
+
+variable "virtual_network_address_space" {
+  description = "Address space assigned to the Virtual Network."
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet."
+  type        = string
+}
+
+variable "subnet_address_prefixes" {
+  description = "Address prefixes assigned to the subnet."
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags assigned to Azure resources."
+  type        = map(string)
+
+  default = {
+    Environment = "Lab"
+    ManagedBy   = "Terraform"
+  }
+}
