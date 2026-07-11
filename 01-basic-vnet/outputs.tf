ae333d37b7abcedc5d602b181839e3a3e499216c
Nathan Z . Albert <76236654+nathanzalbert@users.noreply.github.com>
2026-07-10 22:23:19 -0400

    Add outputs for resource group and virtual network

--- 01-basic-vnet/outputs.tf
+++ 01-basic-vnet/outputs.tf
@@ -1,1 +1,29 @@
+output "resource_group_name" {
+  description = "Name of the created Resource Group."
+  value       = azurerm_resource_group.main.name
+}
 
+output "resource_group_id" {
+  description = "Resource ID of the created Resource Group."
+  value       = azurerm_resource_group.main.id
+}
+
+output "virtual_network_name" {
+  description = "Name of the created Virtual Network."
+  value       = azurerm_virtual_network.main.name
+}
+
+output "virtual_network_id" {
+  description = "Resource ID of the created Virtual Network."
+  value       = azurerm_virtual_network.main.id
+}
+
+output "subnet_name" {
+  description = "Name of the created subnet."
+  value       = azurerm_subnet.main.name
+}
+
+output "subnet_id" {
+  description = "Resource ID of the created subnet."
+  value       = azurerm_subnet.main.id
+}
